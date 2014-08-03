c5000549c626f9e421a1f4ac96bf4d0d2dc4a4e4
vi <me@vikramverma.com>
2014-08-03 20:58:02 +0800

    Low-level (i.e. XML) access to HTTPS Everywhere rulesets.

---
+++ README.md
@@ -0,0 +1,7 @@
+[https-everywhere-rules-base](https://github.com/fmap/https-everywhere-rules-base)
+==================================================================================
+
+Haskell package providing low-level (i.e. XML) access to [HTTPS Everywhere][1]
+rulesets, versioned consistently with its releases.
+
+  [1]: https://www.eff.org/https-everywhere
---
+++ src/Data/HTTPSEverywhere/Rules/Base.hs
@@ -0,0 +1,23 @@
+module Data.HTTPSEverywhere.Rules.Base (
+  getRule,
+  getRules
+) where
+
+import Prelude hiding (readFile)
+import Data.Functor.Infix ((<$$>))
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO (readFile)
+import System.Directory (getDirectoryContents)
+import System.FilePath.Posix (combine, takeExtension)
+
+import Paths_https_everywhere_rules_base (getDataFileName)
+
+getRule :: FilePath -> IO Text
+getRule = readFile
+
+getRules :: IO [FilePath]
+getRules = getDataFileName [] >>= filter isXML <$$> getAbsoluteDirectoryContents 
+  where isXML = (== ".xml") . takeExtension
+
+getAbsoluteDirectoryContents :: FilePath -> IO [FilePath]
+getAbsoluteDirectoryContents x = combine x <$$> getDirectoryContents x
