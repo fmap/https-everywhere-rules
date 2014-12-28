98ff6f9cda6d68b04ebeb88ede97d323979e9d47
vi <me@vikramverma.com>
2014-12-28 10:12:21 +0000

    Nix: correctly propagate alternative package sets.

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let nixpkgs = (import <nixpkgs> {}).fetchgit {
     rev = "c90400b0d366caef5946995a3db6c16bfe3ef33d";
     sha256 = "fe6c4ff4e731110c8cb13b81000773fee97acdf05becea1bd4ce851beb303a3e";
     fetchSubmodules = true;
-  }) {};
+  }) { inherit pkgs; };
 in pkgs.haskellPackages.buildLocalCabalWithArgs {
   name = "https-everywhere-rules"; src = ./.; 
   args = { inherit httpsEverywhereRulesRaw; };
