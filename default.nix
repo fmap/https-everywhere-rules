c50f8496d3148b64855b61adfedbc48174e4e263
Jayesh Kumar Gupta <mail@rejuvyesh.com>
2015-01-09 02:51:45 +0530

    Wrong url for https-everywhere-rules-raw?
    I think.

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let nixpkgs = (import <nixpkgs> {}).fetchgit {
   sha256 = "17kbhjgrpza61a2y8v1h21p8cmrdpvdajd0zrcb8vh18hw5pqa3i";
 }; in { pkgs ? import nixpkgs {} }: let
   httpsEverywhereRulesRaw = import (pkgs.fetchgit {
-    url = "git://github.com/fmap/https-everywhere-rules.git";
+    url = "git://github.com/fmap/https-everywhere-rules-raw.git";
     rev = "c90400b0d366caef5946995a3db6c16bfe3ef33d";
     sha256 = "fe6c4ff4e731110c8cb13b81000773fee97acdf05becea1bd4ce851beb303a3e";
     fetchSubmodules = true;
